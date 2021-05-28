dc1b202157ebd9e7caa7fa92298818a399fd8d60
Hitesh-Gaikar <84028043+Hitesh-Gaikar@users.noreply.github.com>
2021-05-28 20:37:11 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,54 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity()
+    {
+        this.spt.velocityY=this.spt.velocityY+2;
+	}
+	
+    moveUp(){
+    this.spt.velocityY= -12
+	}
+
+	moveRight(){
+    this.spt.x += 12
+	}
+
+	moveLeft(){
+	this.spt.x -= 12
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
